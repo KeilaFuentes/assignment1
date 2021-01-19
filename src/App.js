64b12d6ece453ffcb27f4c189cdc0a38fe15fd1a
unknown <keilafuentesperaltta@gmail.com>
2021-01-19 14:45:35 -0400

    Step 5- Add state to the App component (=> the username) and pass the username to the UserOutput component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserOutput from './InputComponents/UserOutput';
 import UserInput from './InputComponents/UserInput';
 import './App.css';
 
 
 function App() {
+  const[userState, setUserState]= useState({
+    user:[
+      {name:'Melody'}
+    ],
+    otherState:'some other value'
+  });
+
+
   return (
     <div className="App-header" > 
       <UserOutput 
-      name="keila"
+      name={userState.user[0].name}
       lastname="fuentes"/>
       <UserInput></UserInput>
     </div>
